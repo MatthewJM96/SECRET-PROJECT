77e6e098b96ab089358a60f798be4c49e2112448
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-10-28 16:10:29 +0000

    Add support for targetting x86 and x86_64 architecture on Windows.

--- build.bat
+++ build.bat
@@ -10,6 +10,7 @@ SETLOCAL
 :: Some defaults we will override with settings chosen by caller.
 SET "BUILD_CLEAN=false"
 SET "BUILD_TYPE=Debug"
+SET "BUILD_TARGET=x86"
 
 :: The parameter lists for CMAKE and MAKE (or equivalent build tool).
 SET "CMAKE_PARAMS="
@@ -28,6 +29,7 @@ ECHO "            --clean             | -c       ---   Clean build, removes all 
 ECHO "        CMake flags:\n"
 ECHO "            --release           | -r       ---   Compile in release mode.\n"
 ECHO "            --debug             | -d       ---   Compile in debug mode.\n"
+ECHO "            --x64               | -64      ---   Compile for x64 architecture.\n"
 ECHO "        Make flags:\n"
 ECHO "            --verbose           | -v       ---   Run make with verbose set on.\n"
 ECHO "\n"
@@ -43,6 +45,10 @@ GOTO ParamLoopContinue
 
 :Debug
 SET "BUILD_TYPE=Debug"
+GOTO ParamLoopContinue
+
+:X64
+SET "BUILD_TARGET=x86_64"
 GOTO ParamLoopContinue
 
 :Verbose
@@ -67,6 +73,10 @@ GOTO ParamLoopContinue
         GOTO Debug
     ) ELSE IF "%1"=="--debug" (
         GOTO Debug
+    ) ELSE IF "%1"=="-64" (
+        GOTO X64
+    ) ELSE IF "%1"=="--x64" (
+        GOTO X64
     ) ELSE IF "%1"=="-v" (
         GOTO Verbose
     ) ELSE IF "%1"=="--verbose" (
@@ -91,22 +101,32 @@ IF "%BUILD_TYPE%"=="Debug" (
     SET "PRE_BUILD_PARAMS=%PRE_BUILD_PARAMS% --config Release"
 )
 
+:: Choose generator based on which architecture we are targetting.
+IF "%BUILD_TARGET%"=="x86" (
+    SET 'CMAKE_PARAMS=%CMAKE_PARAMS% -G "Visual Studio 14 2017"'
+) ELSE (
+    SET 'CMAKE_PARAMS=%CMAKE_PARAMS% -G "Visual Studio 14 2017 Win64"'
+)
+
+:: Define build directory based on which architecture we are targetting.
+SET "BUILD=build_%BUILD_TARGET%"
+
 :: If build directory exists and we want a clean build, delete all the contents of the build directory.
 :: In any case, if it doesn't exist, create it.
-IF EXIST "build" (
+IF EXIST "%BUILD%" (
     IF "%BUILD_CLEAN%"=="true" (
-        RMDIR build /S /Q
+        RMDIR %BUILD% /S /Q
     )
 ) ELSE (
-    MKDIR build
+    MKDIR %BUILD%
 )
 
 :: Construct and call cmake command to generate build configuration.
-SET "CMAKE_COMMAND=cmake -H. -Bbuild %CMAKE_PARAMS% -Wno-dev"
+SET "CMAKE_COMMAND=cmake -H. -B%BUILD% %CMAKE_PARAMS% -Wno-dev"
 %CMAKE_COMMAND%
 
 :: Construct and call build command.
-SET "BUILD_COMMAND=cmake --build build %PRE_BUILD_PARAMS% -- %BUILD_PARAMS%"
+SET "BUILD_COMMAND=cmake --build %BUILD% %PRE_BUILD_PARAMS% -- %BUILD_PARAMS%"
 %BUILD_COMMAND%
 
 :: Return to terminal's previous working directory.
