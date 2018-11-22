2f85cf215e67daccf82564daefbb4e20bab73fd5
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2018-11-22 22:39:32 +0000

    Create a standard set of includes.

---
+++ include/stdafx.h
@@ -0,0 +1,18 @@
+// Containers
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <tuple>
+
+// Strings
+#include <cstring>
+#include <string>
+
+// Graphics
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
+// Maths
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
